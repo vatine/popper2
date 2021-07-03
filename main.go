e4f29e423373adea221d29b6d5f83f472417a35e
Ingvar Mattsson <imagineaclevernamehere@gmail.com>
2021-07-03 22:03:14 +0200

    Make debug logs and debug mode flag-controlled.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flags"
 	"fmt"
 	
 	"github.com/hajimehoshi/ebiten/v2"
@@ -10,9 +11,15 @@ import (
 )
 
 func main() {
-	log.SetLevel(log.DebugLevel)
+	verbose := flags.Bool("v", false, "Verbose (dbug) logging")
+	testMode := flags.Bool("d", false, "Debug (test) mode")
 	g := game.NewGame()
-	// g.TestSetup()
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+	if *testMode {
+		g.TestSetup()
+	}
 
 	fmt.Println(ebiten.RunGame(g))
 }
